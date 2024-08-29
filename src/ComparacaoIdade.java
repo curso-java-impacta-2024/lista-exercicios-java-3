d59214a3995fb588c080924e5ce3704542b7f70a
MarianaGaldino <marianagaldino1@gmail.com>
2024-08-29 20:57:30 -0300

    adicionando estrutura condicional if/else

--- src/ComparacaoIdade.java
+++ src/ComparacaoIdade.java
@@ -13,5 +13,12 @@ public class ComparacaoIdade {
 
         entrada.close();
 
+        if(planeta1 > planeta2){
+            System.out.println("O planeta 1 é mais antigo");
+        }else if(planeta1 < planeta2){
+            System.out.println("O planeta 2 é mais antigo");
+        } else {
+            System.out.println("Os dois planetas tem a mesma idade");
+        }
     }
 }
