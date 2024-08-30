ebbc12c44db54af64db2f83f0910fd11374c0565
MarianaGaldino <marianagaldino1@gmail.com>
2024-08-29 21:12:44 -0300

    inicializando exercicio 2

---
+++ src/CalculoDeOrbita.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CalculoDeOrbita {
+    public static void main(String[] args) {
+        double distanciaInicial = 150.0;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o fator de mudança");
+        double fatorMudanca = entrada.nextDouble();
+
+
+    }
+}
